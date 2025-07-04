1363081ee5997819b798f7a414f2c292f88d0ff5
AndrewMaru <Kimo.chu28@gmail.com>
2025-07-04 10:30:01 +0700

    Update application.py

--- application.py
+++ application.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import joblib
 
-joblib.load('model.pkl')
+model = joblib.load('model.pkl')
 
 st.title('🫁 Lung Cancer Diagnosis')
 st.write("Please fill out the following information to assess the likelihood of lung cancer.")
